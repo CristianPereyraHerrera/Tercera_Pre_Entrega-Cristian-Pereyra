a1122c0b85122d1052ae368c38bbaf63b73433e3
Cristian Pereyra <cristianniky@gmail.com>
2023-03-16 00:22:57 -0300

    Se agregaron las tablas en la DB

--- AppEdukate/models.py
+++ AppEdukate/models.py
@@ -8,3 +8,34 @@ class Course(models.Model):
 
     def __str__(self):
         return f"Curso: {self.name} {self.commission}"
+
+
+class Delivery(models.Model):
+    name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    course = models.CharField(max_length=30)
+    commission = models.IntegerField(unique=True)
+    delivery_date = models.DateField()
+    delivered = models.BooleanField()
+
+    def __str__(self):
+        return f"Delivery: {self.name} {self.last_name} {self.course} {self.commission} {self.delivery_date} {self.delivered}"
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"Student: {self.name} {self.last_name} {self.email}"
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    profession = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"Teacher: {self.name} {self.last_name} {self.email} {self.profession}"
--- AppEdukate/views.py
+++ AppEdukate/views.py
@@ -11,6 +11,10 @@ def about(request):
     return render(request, "AppEdukate/about.html")
 
 
+def contact(request):
+    return render(request, "AppEdukate/contact.html")
+
+
 def course(request):
     return render(request, "AppEdukate/course.html")
 
@@ -21,6 +25,22 @@ def courses(request):
         "cursos": all_courses
     }
     return render(request, "AppEdukate/courses.html", context=context)
+
+
+def detail(request):
+    return render(request, "AppEdukate/detail.html")
+
+
+def feature(request):
+    return render(request, "AppEdukate/feature.html")
+
+
+def team(request):
+    return render(request, "AppEdukate/team.html")
+
+
+def testimonial(request):
+    return render(request, "AppEdukate/testimonial.html")
 
 
 def students(request):
